0c68b41ca0b0479e5679c7fe60190754def9dc6e
martiera <martins.ierags@gmail.com>
2017-01-06 11:14:32 +0200

    Setup service daemon and logrotate

---
+++ install/setup.sh
@@ -0,0 +1,5 @@
+sudo cp ./logrotate /etc/logrotate.d/gateway
+sudo cp ./gateway.service /etc/systemd/system/gateway.service
+sudo systemctl daemon-reload
+sudo systemctl start gateway.service
+sudo systemctl enable gateway.service
